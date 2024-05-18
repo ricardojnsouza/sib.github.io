f6cc05089362cf3e53c6630855c0710831b92b5e
Ricardo Souza <ricardojnsouza@hotmail.com>
2024-05-18 18:16:31 -0300

    Alteração do Index para o Instagram

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Links</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+
+
+<body>
+    <div>
+        <h1>Os instagrans do Sentimento</h1>
+    </div>
+
+
+    <div class="container py-5 shadow-lg">
+        <main>
+
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Departamento</th>
+                        <th scope="col">Responsável</th>
+                        <th scope="col">Instagram</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Unijovem</td>
+                        <td>A de Amor</td>
+                        <td><a class="icon-link" href="https://www.instagram.com/unijov">
+                                <svg class="bi" aria-hidden="true">
+                                    <use xlink:href="#box-seam"></use>
+                                </svg>
+                                Instagram
+                            </a></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Embaixadores</td>
+                        <td>B de Baixinho</td>
+                        <td><a class="icon-link" href="https://www.instagram.com/embaixadasibbrasiliateimosa">
+                                <svg class="bi" aria-hidden="true">
+                                    <use xlink:href="#box-seam"></use>
+                                </svg>
+                                Instagram
+                            </a></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Mensageiras</td>
+                        <td>C de Coração</td>
+                        <td><a class="icon-link" href="https://www.instagram.com/mensageiras_sibbt">
+                                <svg class="bi" aria-hidden="true">
+                                    <use xlink:href="#box-seam"></use>
+                                </svg>
+                                Instagram
+                            </a></td>
+                    </tr>
+                </tbody>
+            </table>
+
+
+        </main>
+    </div>
+</body>
+
+</html>
